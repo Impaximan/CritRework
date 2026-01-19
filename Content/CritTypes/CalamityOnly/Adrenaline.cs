1b3762411497bca6049fcda3645b61c51f02f3c9
Impaximan <jmwallace23@outlook.com>
2026-01-18 23:31:33 -0500

    Added calamity cross-mod conditions

---
+++ Content/CritTypes/CalamityOnly/Adrenaline.cs
@@ -0,0 +1,41 @@
+﻿using CritRework.Common.ModPlayers;
+using MonoMod.RuntimeDetour;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Reflection;
+using Terraria;
+using CritRework.Common.Globals;
+
+namespace CritRework.Content.CritTypes.RandomPool
+{
+    [JITWhenModsEnabled("CalamityMod")]
+    internal class Adrenaline : CritType
+    {
+        public override bool InRandomPool => true;
+
+        public override bool ShouldLoad()
+        {
+            return ModLoader.HasMod("CalamityMod");
+        }
+
+        public override bool ShowWhenActive => true;
+
+        public override float GetDamageMult(Player Player, Item Item) => 1.6f;
+
+        public override bool ShouldCrit(Player Player, Item Item, Projectile? Projectile, NPC target, NPC.HitModifiers modifiers)
+        {
+            if (ModLoader.TryGetMod("CalamityMod", out Mod Calamity))
+            {
+                ModPlayer mp = Player.GetModPlayer(Calamity.Find<ModPlayer>("CalamityPlayer"));
+
+                if(mp.GetType().GetField("adrenalineModeActive").GetValue(mp) is bool adrenaline)
+                {
+                    return adrenaline;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Content/CritTypes/CalamityOnly/StealthStrike.cs
@@ -0,0 +1,56 @@
+﻿
+using CritRework.Common.Globals;
+
+namespace CritRework.Content.CritTypes.RandomPool
+{
+    [JITWhenModsEnabled("CalamityMod")]
+    internal class StealthStrike : CritType
+    {
+        public override bool InRandomPool => true;
+
+        public override bool ShouldLoad()
+        {
+            return ModLoader.HasMod("CalamityMod");
+        }
+
+        public override bool ShowWhenActive => true;
+
+        public override float GetDamageMult(Player Player, Item Item) => 1.35f;
+
+        public override bool CanApplyTo(Item item)
+        {
+            if (ModLoader.TryGetMod("CalamityMod", out Mod Calamity))
+            {
+                if (item.DamageType.CountsAsClass(Calamity.Find<DamageClass>("RogueDamageClass")))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public override bool ShouldCrit(Player Player, Item Item, Projectile? Projectile, NPC target, NPC.HitModifiers modifiers)
+        {
+            if (ModLoader.TryGetMod("CalamityMod", out Mod Calamity))
+            {
+                if (Projectile == null)
+                {
+                    ModPlayer mp = Player.GetModPlayer(Calamity.Find<ModPlayer>("CalamityPlayer"));
+
+                    if (mp.GetType().GetField("rogueStealth").GetValue(mp) is float a && mp.GetType().GetField("rogueStealthMax").GetValue(mp) is float b)
+                    {
+                        return a >= b;
+                    }
+                }
+                if (Projectile.TryGetGlobalProjectile(Calamity.Find<GlobalProjectile>("CalamityGlobalProjectile"), out var p))
+                {
+                    if (p.GetType().GetField("stealthStrike").GetValue(p) is bool b)
+                    {
+                        return b;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
