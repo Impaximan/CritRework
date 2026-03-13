d322f178548c7b0ad5cdb1766eb55e45ebc624ac
Impaximan <jmwallace23@outlook.com>
2026-03-13 11:28:45 -0400

    Added Deliberate

---
+++ Content/Prefixes/Weapon/Deliberate.cs
@@ -0,0 +1,23 @@
+﻿
+namespace CritRework.Content.Prefixes.Weapon
+{
+    public class Deliberate : ModPrefix
+    {
+        public override PrefixCategory Category => PrefixCategory.AnyWeapon;
+
+        public override bool CanRoll(Item item)
+        {
+            if (item.channel)
+            {
+                return false;
+            }
+            return base.CanRoll(item);
+        }
+
+        public override void SetStats(ref float damageMult, ref float knockbackMult, ref float useTimeMult, ref float scaleMult, ref float shootSpeedMult, ref float manaMult, ref int critBonus)
+        {
+            critBonus = 50;
+            useTimeMult = 1.25f;
+        }
+    }
+}
