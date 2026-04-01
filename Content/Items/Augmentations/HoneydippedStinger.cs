5695c294cff2148cdc96fcdfebf6019f4e0b20d1
Impaximan <jmwallace23@outlook.com>
2026-03-31 22:02:48 -0400

    Added Honeydipped Stinger

---
+++ Content/Items/Augmentations/HoneydippedStinger.cs
@@ -0,0 +1,70 @@
+﻿using CritRework.Common.ModPlayers;
+using System;
+using Terraria.Audio;
+
+namespace CritRework.Content.Items.Augmentations
+{
+    public class HoneydippedStinger : Augmentation
+    {
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.BeeWax, 5)
+                .AddIngredient(ItemID.Stinger, 3)
+                .AddIngredient(ItemID.HoneyBlock, 2)
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 26;
+            Item.height = 34;
+            Item.UseSound = equipSound;
+            Item.rare = ItemRarityID.Orange;
+            Item.value = Item.sellPrice(0, 3, 0, 0);
+        }
+
+        public override bool OverrideNormalCritBehavior(Player player, Item item, Projectile projectile, NPC.HitModifiers modifiers, CritType critType, NPC target)
+        {
+            modifiers.DisableCrit();
+            return true;
+        }
+
+        static float counter = 0f;
+
+        public override void AugmentationOnHitNPC(Player player, Item item, Projectile projectile, NPC.HitInfo hit, CritType critType, NPC target)
+        {
+            if (target.type == NPCID.TargetDummy || target.SpawnedFromStatue)
+            {
+                return;
+            }
+
+            NPC.HitModifiers modifiers = new NPC.HitModifiers();
+            if (critType.ShouldCrit(player, item, projectile, target, modifiers, item.IsSpecial()))
+            {
+                float damageMult = critType.GetDamageMult(player, item);
+
+                player.GetModPlayer<CritPlayer>().timeSinceCrit = 0;
+
+                SoundEngine.PlaySound(new SoundStyle("CritRework/Assets/Sounds/SharpHit2")
+                {
+                    PitchVariance = 0.5f,
+                    Volume = 1.5f
+                }, target.Center);
+
+                counter += (damageMult - 1f) * 2.5f; //The amount healed per crit
+
+                if ((int)counter > 0)
+                {
+                    if (!player.moonLeech)
+                    {
+                        player.Heal((int)counter);
+                        if (Main.netMode != NetmodeID.SinglePlayer) NetMessage.SendData(MessageID.PlayerHeal, number: player.whoAmI, number2: (int)counter);
+                    }
+                    counter -= (int)counter;
+                }
+            }
+        }
+    }
+}
