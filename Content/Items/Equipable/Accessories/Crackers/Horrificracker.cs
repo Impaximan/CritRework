bdd97f58a07432ddb950b78e643a7785676adc39
Impaximan <jmwallace23@outlook.com>
2026-04-05 10:17:18 -0700

    Added Horrificracker

---
+++ Content/Items/Equipable/Accessories/Crackers/Horrificracker.cs
@@ -0,0 +1,43 @@
+﻿using CritRework.Common.Globals;
+using CritRework.Common.ModPlayers;
+using System.Collections.Generic;
+
+namespace CritRework.Content.Items.Equipable.Accessories.Crackers
+{
+    public class Horrificracker : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 22;
+            Item.height = 40;
+            Item.accessory = true;
+            Item.rare = ItemRarityID.Cyan;
+            Item.value = Item.buyPrice(0, 40, 0, 0);
+            Item.GetGlobalItem<CritItem>().forceCanCrit = true;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient<WiseCracker>()
+                .AddIngredient(ItemID.NecromanticScroll)
+                .AddTile(TileID.TinkerersWorkbench)
+                .Register();
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            player.AddEquip<WiseCracker>();
+            player.maxMinions++;
+        }
+
+        public override bool CanAccessoryBeEquippedWith(Item equippedItem, Item incomingItem, Player player)
+        {
+            if ((equippedItem.type == ItemID.NecromanticScroll && incomingItem.type == Type)  || (equippedItem.type == Type && incomingItem.type == ItemID.NecromanticScroll))
+            {
+                return false;
+            }
+            return !equippedItem.IsCracker() || !incomingItem.IsCracker();
+        }
+    }
+}
